6cd08c0fef2a5f110fe0bf091581f85a133d0f1f
Devcloser <Devcloser@users.noreply.github.com>
2021-08-07 11:07:03 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.bfafb3e49131bc9959fcbd49a3d35ff2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327912394c7a7ea9aa6954947f81f7ba",
+    "url": "https://devcloser.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "f537f9da031df5cd4d14",
+    "url": "https://devcloser.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "6d0b8abde5e8be6992d6",
+    "url": "https://devcloser.github.io/task-management-frontend/static/js/2.fbe8c191.chunk.js"
+  },
+  {
+    "revision": "f537f9da031df5cd4d14",
+    "url": "https://devcloser.github.io/task-management-frontend/static/js/main.852c7fcf.chunk.js"
+  },
+  {
+    "revision": "90a395afc99a29095757",
+    "url": "https://devcloser.github.io/task-management-frontend/static/js/runtime~main.0466b83a.js"
+  }
+]);
